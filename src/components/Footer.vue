9c3977095bf929d2be6d35da0bdfe2b20a95abb0
MichaelLuo520 <po2319987@gmail.com>
2021-10-14 16:14:25 +0800

    var variables colors

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="container-fluid"></footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss">
+</style>
