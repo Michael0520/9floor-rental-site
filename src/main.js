fa7d84cc7bf57a50405923af285b7c7174ee79e1
MichaelLuo520 <po2319987@gmail.com>
2021-10-25 20:54:48 +0800

    install axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import axios from "axios";
 import { createApp } from "vue";
+import VueAxios from "vue-axios";
 import App from "./App.vue";
 import router from "./router";
 
 createApp(App)
   .use(router)
+  .use(VueAxios, axios)
   .mount("#app");
