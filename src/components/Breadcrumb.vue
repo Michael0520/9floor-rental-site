71c66efc7e11d8bc390019200fad5e19e6d72853
MichaelLuo520 <po2319987@gmail.com>
2021-10-29 15:27:08 +0800

    update router-link to components/Breadcrumb

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -6,7 +6,7 @@
     >
         <ol class="breadcrumb">
             <li class="breadcrumb-item">
-                <a href="#">
+                <router-link to="/">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="14"
@@ -18,13 +18,13 @@
                         <path
                             d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"
                         /></svg
-                ></a>
+                ></router-link>
             </li>
             <li class="breadcrumb-item active" aria-current="page">
-                <a href="#">入住</a>
+                <router-link to="/">入住</router-link>
             </li>
             <li class="breadcrumb-item active" aria-current="page">
-                <a href="#">房源搜尋</a>
+                <router-link to="/filter">房源搜尋</router-link>
             </li>
         </ol>
     </nav>
@@ -34,7 +34,7 @@
 export default {
     data() {
         return {};
-    },
+    }
 };
 </script>
 
