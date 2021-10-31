776c2292dd697275e506073efb5bb4296f5067fe
MichaelLuo520 <po2319987@gmail.com>
2021-10-31 18:46:57 +0800

    add rightSiderBar

--- src/views/About.vue
+++ src/views/About.vue
@@ -107,12 +107,7 @@
                     </section>
                 </div>
                 <!-- left-sider-bar -->
-                <div
-                    class="index_side-bar index_side-bar-right"
-                    :class="isTop ? 'index_fixed-side-bar' : ''"
-                >
-                    <div class="index_side-bar-content index_fade-in-out"></div>
-                </div>
+                <rightSiderBar />
             </div>
         </div>
         <Footer />
@@ -126,6 +121,7 @@ import Footer from "../components/Footer.vue";
 import SiderBar from "../components/SiderBar.vue";
 import WhiteNavBar from "../components/WhiteNavBar.vue";
 import Breadcrumb from "../components/Breadcrumb.vue";
+import rightSiderBar from "../components/rightSiderBar.vue";
 
 export default {
     data() {
@@ -172,6 +168,7 @@ export default {
         WhiteNavBar,
         Breadcrumb,
         SiderBar,
+        rightSiderBar,
         Footer
     }
 };
@@ -367,59 +364,6 @@ export default {
     }
     .index_section-title.mobile-show {
         display: block;
-    }
-}
-
-// side-bar
-.index_side-bar {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 60px;
-    border-right: 1px solid $info;
-    border-top: 1px solid $info;
-    opacity: 1;
-    transition: opacity 0.5s;
-    flex-basis: 60px;
-    flex-shrink: 0;
-    &:last-child {
-        border-right: 0;
-        border-left: 1px solid $info;
-    }
-    .index_side-bar-content {
-        margin-top: 24px;
-        transition: opacity 0.3s;
-        opacity: 1;
-        .index_vertical-word {
-            display: flex;
-            flex-direction: column;
-            margin-bottom: 20px;
-            span {
-                margin-bottom: 2px;
-                font-size: 10px;
-                color: $primary;
-                transform: rotate(90deg);
-            }
-        }
-        .index_vertical-en {
-            font-size: 10px;
-            color: $primary;
-            writing-mode: vertical-lr;
-        }
-    }
-    @include lg {
-        display: none;
-    }
-}
-.index_side-bar-left,
-.index_side-bar-right {
-    flex-basis: 60px;
-}
-.index_side-bar-left {
-    div {
-        &:first-child {
-            margin-top: 60px;
-        }
     }
 }
 </style>
