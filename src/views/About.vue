012ba1be4e22a8f87adfeceb33991c26140ba720
MichaelLuo520 <po2319987@gmail.com>
2021-10-31 11:08:47 +0800

    first init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,48 @@
 <template>
     <div class="about"></div>
+    <NavBar />
+    <SiderBar />
+    <Footer />
 </template>
 
+
+
+<script>
+import Footer from "../components/Footer.vue";
+import SiderBar from "../components/SiderBar.vue";
+import NavBar from "../components/NavBar.vue";
+
+export default {
+    data() {
+        return {
+            name: "HomePage",
+            isTop: false
+        };
+    },
+    mounted() {
+        // if isTop = 0 , add fixed class , else remove
+        window.addEventListener("scroll", () => {
+            let clientTop =
+                document.documentElement.scrollTop ||
+                document.body.scrollTop ||
+                window.pageYOffset;
+            this.scrollNum = clientTop;
+            console.log(`目前頁面高度 ${clientTop}`);
+            if (clientTop >= 900) {
+                this.isTop = true;
+            } else {
+                this.isTop = false;
+            }
+        });
+    },
+    components: {
+        NavBar,
+        SiderBar,
+        Footer
+    }
+};
+</script>
 <style lang="scss">
+@import "~bootstrap/dist/css/bootstrap.css";
 @import "../assets/custom.scss";
 </style>
