90d49cdef8768f30e69711e16915c046f84d4e0c
MichaelLuo520 <po2319987@gmail.com>
2021-10-31 18:50:26 +0800

    create new Page ColivingPage

---
+++ src/views/Coliving.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <white-nav-bar />
+        <Breadcrumb />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import WhiteNavBar from "../components/WhiteNavBar.vue";
+import Breadcrumb from "../components/Breadcrumb.vue";
+import Footer from "../components/Footer.vue";
+export default {
+    components: { WhiteNavBar, Breadcrumb, Footer },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/dist/css/bootstrap.css";
+@import "../assets/custom.scss";
+</style>
