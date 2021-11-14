fcbf10908100eb8f228ec0572767a0a07d7f5efe
MichaelLuo520 <po2319987@gmail.com>
2021-11-14 18:52:38 +0800

    add loading-overlay plugin import

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import axios from "axios";
 import "bootstrap";
 import { createApp } from "vue";
 import VueAxios from "vue-axios";
+import VueLoading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 import App from "./App.vue";
 import router from "./router";
 
 createApp(App)
   .use(router)
   .use(VueAxios, axios)
+  .use(VueLoading)
   .mount("#app");
