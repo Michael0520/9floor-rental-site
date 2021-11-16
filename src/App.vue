1f5db6f4bc9a7773c2bbe46d6a04d412e361c671
MichaelLuo520 <po2319987@gmail.com>
2021-11-16 10:59:56 +0800

    remove a style

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,5 @@ export default {};
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-
-    a {
-        text-decoration: none;
-    }
 }
 </style>
