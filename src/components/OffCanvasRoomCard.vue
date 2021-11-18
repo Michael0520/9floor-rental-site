f78470d5b495a1886c014e70804a606d93632f87
MichaelLuo520 <po2319987@gmail.com>
2021-11-18 12:34:56 +0800

    components by offCanvasRoomCard

---
+++ src/components/OffCanvasRoomCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <hr />
+        </div>
+        <div class="col-12">
+            <div class="d-flex justify-content-end">
+                <button class="index_button-icon index_remove-button">
+                    <i class="material-icons index_icon">close</i>
+                </button>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="d-flex py-2">
+                <div class="text-success me-2 fs-4 fw-bold">
+                    仁愛
+                </div>
+                <div class="fs-4 fw-lighter ">
+                    仁愛 RoomC
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="d-flex justify-content-between py-1">
+                <div>雅房</div>
+                <div>招募中</div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="py-1">NTD <span>13,500</span> / 月</div>
+        </div>
+    </div>
+</template>
