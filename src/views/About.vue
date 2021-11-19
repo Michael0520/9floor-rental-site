4a0fc968a51148089eb025597b2b94ede95c31ec
MichaelLuo520 <po2319987@gmail.com>
2021-11-19 09:06:00 +0800

    remove local variable scss

--- src/views/About.vue
+++ src/views/About.vue
@@ -184,7 +184,7 @@ export default {
 };
 </script>
 <style lang="scss">
-@import "~bootstrap/dist/css/bootstrap.css";
+// @import "~bootstrap/dist/css/bootstrap.css";
 @import "../assets/custom.scss";
 
 .index_aboutSection {
